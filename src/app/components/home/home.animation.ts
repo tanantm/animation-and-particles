a224733496ce02b94c2d550a56b39700c06cf297
tanantm <ekanu20023@yahoo.co.jp>
2021-05-04 18:17:55 +0900

    homeコンポーネントの追加

---
+++ src/app/components/home/home.animation.ts
@@ -0,0 +1,34 @@
+import { GetjsonService } from './../../services/get-json-data/get-json.service';
+import { trigger, style, transition, animate, keyframes } from '@angular/animations';
+
+export module HomeAnimation {
+
+  export const statusChange = trigger('statusChange', [
+    transition('* <=> loop-state', [
+      animate('60s ease-in-out', keyframes(
+        setStyle()
+      ),
+      )])
+  ]);
+
+  function getStyle(getjsonService: GetjsonService) {
+    return getjsonService.getJsonData()?.[1];
+  }
+
+  function setStyle() {
+    // jsonファイルの指定したスタイル設定を返す
+    const styleList: any = [];
+    const target = getStyle(new GetjsonService);
+
+    if (target) {
+      [...Object.values(target), ...Object.values(target).reverse()].forEach(item => {
+        styleList.push(style(item));
+      });
+    }
+
+    return styleList;
+  }
+}
+
+
+
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HomeAnimation } from './home.animation';
+import { AnimationEvent } from '@angular/animations';
+import { GetjsonService } from './../../services/get-json-data/get-json.service';
+// import particle from '../../config/particles.json';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  animations: [HomeAnimation.statusChange],
+  encapsulation: ViewEncapsulation.None
+})
+export class HomeComponent implements OnInit {
+  statusState = 'start-state';
+  isLoop = false;
+  bgcolor: any = {};
+  id = "tsparticles";
+  // particlesOptions = particle;
+
+  constructor(private getjsonService: GetjsonService) { }
+
+  ngOnInit(): void {
+    this.onLoopState();
+  }
+
+  onAnimationStarted(event: AnimationEvent): void {
+    this.bgcolor = this.getjsonService.getBackColor()?.[1];
+  }
+
+  onAnimationDone(event: AnimationEvent): void {
+    if (this.isLoop) {
+      setTimeout(() => {
+        this.statusState === 'start-state' ? this.statusState = 'loop-state' : this.statusState = 'start-state';
+      }, 0);
+    }
+  }
+
+  onLoopState() {
+    this.isLoop = true;
+    this.statusState = 'loop-state';
+  }
+}
